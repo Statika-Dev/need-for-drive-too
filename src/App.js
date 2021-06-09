28ec8c55b2d425a7e5d8d57f99915c3b89b64b99
Statika <svetlanakol68@gmail.com>
2021-06-09 17:50:54 +0300

    add burger menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import burgerMenu from'./menu_btn.svg';
+import Menu from './burgerMenu/Menu';
+
+
 
 function App() {
+
+  const [menuActive, setMenuActive] = useState(false)
+
+  const items = [{value:"парковка", href:"/parkovka"},
+  {value:"страховка", href:"/strahovka"},
+  {value:"бензин", href:"/benzin"},
+  {value:"обслуживание", href:"/obcluzivanie"}]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="nav">
+        <div className="burger-btn" onClick={() => setMenuActive(!menuActive)}>
+          <img src={burgerMenu} alt="no download"></img>
+        </div>
+      </div>
+      
+      <Menu active = {menuActive} setActive = {setMenuActive} items={items}/>
     </div>
   );
 }
